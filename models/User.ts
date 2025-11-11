1a69cd4da890634c23fb12d98b5585cfad57acba
THOMASUKUTTY REJI <thomasukuttyreji7@gmail.com>
2025-11-11 14:49:21 +0530

    bug fixed 1.2

--- models/User.ts
+++ models/User.ts
@@ -1,6 +1,7 @@
 import mongoose, { Schema, Document, Model } from 'mongoose';
 
 export interface IUser extends Document {
+  _id: mongoose.Types.ObjectId;
   name: string;
   email: string;
   password?: string;
