aec2f8cba7d848517c2f94b9909a0e8169aa0e9d
THOMASUKUTTY REJI <thomasukuttyreji7@gmail.com>
2025-11-12 11:43:52 +0530

    stable v 1.1

---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+import LoadingScreen from '@/components/LoadingScreen';
+
+export default function Loading() {
+  return <LoadingScreen message="Loading..." fullScreen={true} />;
+}
+
+
--- components/ui/avatar.tsx
+++ components/ui/avatar.tsx
@@ -13,7 +13,7 @@ function Avatar({
     <AvatarPrimitive.Root
       data-slot="avatar"
       className={cn(
-        "relative flex size-8 shrink-0 overflow-hidden rounded-full",
+        "relative flex size-8 shrink-0 overflow-hidden rounded-full aspect-square",
         className
       )}
       {...props}
@@ -28,7 +28,7 @@ function AvatarImage({
   return (
     <AvatarPrimitive.Image
       data-slot="avatar-image"
-      className={cn("aspect-square size-full object-cover", className)}
+      className={cn("aspect-square size-full object-cover object-center rounded-full", className)}
       {...props}
     />
   )
